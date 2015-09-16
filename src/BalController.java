218d49684a8df9a2dd5bac227309c2d2022f1413
Michael <michael1vk@hotmail.com>
2015-09-16 18:02:22 +0200

    Balcontroller constructor aangemaakt

--- src/BalController.java
+++ src/BalController.java
@@ -1,4 +1,16 @@
 
 public class BalController {
 
+    private Bal bal;
+    private BalView balView;
+    private ValBewegingPaneel valBewegingPaneel;
+    private ControlePaneelNoord noordpaneel;
+
+    public BalController(Bal bal, BalView balview, ValBewegingPaneel valBewegingPaneel,
+        ControlePaneelNoord noordpaneel) {
+        this.bal = bal;
+        this.balView = balview;
+        this.valBewegingPaneel = valBewegingPaneel;
+        this.noordpaneel = noordpaneel;
+    }
 }
