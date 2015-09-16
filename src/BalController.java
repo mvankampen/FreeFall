524ca3ce8f9f972f1d76c1608a0d2feae2ae058c
Michael <michael1vk@hotmail.com>
2015-09-16 18:13:48 +0200

    Aangepaste ControlePaneelZuid

--- src/BalController.java
+++ src/BalController.java
@@ -1,10 +1,18 @@
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 
-public class BalController {
+public class BalController implements Runnable, MouseWheelListener {
 
     private Bal bal;
     private BalView balView;
     private ValBewegingPaneel valBewegingPaneel;
     private ControlePaneelNoord noordpaneel;
+
+    private boolean doorgaan_thread;
+    private boolean doorgaan_wheel;
+    private int dt;
+    private double valhoogte;
+    private Thread draad;
 
     public BalController(Bal bal, BalView balview, ValBewegingPaneel valBewegingPaneel,
         ControlePaneelNoord noordpaneel) {
@@ -12,5 +20,46 @@ public class BalController {
         this.balView = balview;
         this.valBewegingPaneel = valBewegingPaneel;
         this.noordpaneel = noordpaneel;
+    }
+
+    /**
+     * Invoked when the mouse wheel is rotated.
+     *
+     * @param e
+     * @see MouseWheelEvent
+     */
+    @Override public void mouseWheelMoved(MouseWheelEvent e) {
+
+    }
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override public void run() {
+
+    }
+
+    public void pleaseStart() {
+
+    }
+
+    public void pleaseStop() {
+
+    }
+
+    private void slaap(int msec) {
+        try {
+            Thread.sleep(msec);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/ControlePaneelZuid.java
+++ src/ControlePaneelZuid.java
@@ -1,3 +1,4 @@
+import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -7,16 +8,25 @@ import javafx.scene.layout.HBox;
  * Created by Michael on 10-09-15.
  */
 public class ControlePaneelZuid extends HBox {
+    @FXML
+    private Button animatieBtn;
+    @FXML
+    private Button stopBtn;
+    @FXML
+    private Button resetBtn;
+
+    private Bal bal;
+    private BalView balView;
     private BalController balController;
-    private Button animatieBtn, stopBtn, resetBtn;
 
-    public ControlePaneelZuid(BalController balController) {
-        this.balController = balController;
-        setAlignment(Pos.CENTER);
+    public ControlePaneelZuid(Bal bal, BalView balview, BalController controller) {
+        this.bal = bal;
+        this.balView = balview;
+        this.balController = controller;
         this.animatieBtn = new Button("Animatie");
+        this.animatieBtn.setOnAction(event -> this.balController.pleaseStart());
         this.stopBtn = new Button("Stop");
+        this.stopBtn.setOnAction(event -> this.balController.pleaseStop());
         this.resetBtn = new Button("Reset");
-
-        getChildren().addAll(animatieBtn,stopBtn,resetBtn);
     }
 }
