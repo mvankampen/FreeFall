6e545eb47af8b7c23164701dcf1c29fe309cb00f
dennis <dennissloove1995@hotmail.nl>
2015-09-16 17:45:02 -0700

    panelen

--- src/ControlePaneelNoord.java
+++ src/ControlePaneelNoord.java
@@ -1,4 +1,27 @@
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
-public class ControlePaneelNoord {
+/**
+ * Created by dennis on 16-09-15.
+ */
+public class ControlePaneelNoord extends HBox {
+    private BalController balController;
+    private Label labelBereikX;
+    private Label labelBereikY;
+    private TextField bereikXveld;
+    private TextField bereikYveld;
 
+    public NorthPane(BalController balController) {
+        this.balController = balController;
+        this.labelBereikX = new Label("Bereik x:");
+        this.labelBereikY = new Label("Bereik y:");
+        this.bereikXveld = new TextField();
+        this.bereikYveld = new TextField();
+        setAlignment(Pos.CENTER);
+        this.setSpacing(10);
+        this.getChildren().addAll(labelBereikX,bereikXveld, labelBereikY, bereikYveld);
+
+    }
 }
--- src/ControlePaneelZuid.java
+++ src/ControlePaneelZuid.java
@@ -1,4 +1,22 @@
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
-public class ControlePaneelZuid {
+/**
+ * Created by dennis on 10-09-15.
+ */
+public class ControlePaneelZuid extends HBox {
+    private BalController balController;
+    private Button animatieBtn, stopBtn, resetBtn;
 
+    public SouthPane(BalController balController) {
+        this.balController = balController;
+        setAlignment(Pos.CENTER);
+        this.animatieBtn = new Button("Animatie");
+        this.stopBtn = new Button("Stop");
+        this.resetBtn = new Button("Reset");
+
+        getChildren().addAll(animatieBtn,stopBtn,resetBtn);
+    }
 }
